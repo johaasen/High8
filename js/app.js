97064f96463418a89bf5760971df6dc780964752
Johannes Haasen <jo.haasen@gmail.com>
2014-05-31 11:59:36 +0200

    test backend page

--- js/app.js
+++ js/app.js
@@ -5,19 +5,35 @@ var app = angular.module('Mampf-Angular', [
 ]);
 
 app.config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(false); //TODO effects of (true)?
+  $locationProvider.html5Mode(false);
+  //TODO effects of (true)?
   $routeProvider.when('/',          {templateUrl: "home.html"});
   $routeProvider.when('/details',    {templateUrl: "details.html"});
+  $routeProvider.when('/testBackend',    {templateUrl: "testBackend.html"});
 });
 
 /* 
-  TODO: To be discussed:
+  TODO: 
+  General app.js discussion
   - encapsulation of MampfAPI and config object like currently implemented useful?
   - usage of angular.factory vs. angular.service
 */
 
+/*
+//TODO: separate MampfAPI service necessary?
+app.service('MampfConnection', function(){
+  var url = "http://dennistempi.itm.uni-luebeck.de/mampf/";
+  var api = new MampfAPI(url);
+});
+*/
+
 app.service('MampfConfig', function() {
   // model for configuration of MampfAPI
+
+  // TODO:
+  // - remove this.config and use this.identity etc. directly
+  // - checks for duplicate invitees/timeslots needed
+  // - md5 encoding for identity and invitees - should it be done here?
   this.config = {};
   this.config.identity = "";
   this.config.invitees = [];
@@ -47,10 +63,14 @@ app.service('MampfConfig', function() {
 
   this.delTimeslots = function() { this.config.timeslots = []; };
   this.getTimeslots = function() { return this.config.timeslots; };
-  this.addTimeslot = function(timeslot) { this.config.timeslots.push(timeslot); };
+  this.addTimeslot = function(timeslot) {
+    //this.config.timeslots.push(timeslot);
+    // adds reference to input fields(?), workaround:
+    this.config.timeslots.push({"startTime": timeslot.startTime, "endTime":timeslot.endTime});
+  };
   this.remTimeslot = function(timeslot) {
     for (var pos in this.config.timeslots) {
-      if (this.config.timeslots[pos].hasOwnProperty("startTime") && this.config.timeslots[pos].hasOwnProperty("startTime")){
+      if (this.config.timeslots[pos].hasOwnProperty("startTime") && this.config.timeslots[pos].hasOwnProperty("endTime")){
         if(this.config.timeslots[pos].startTime === timeslot.startTime && this.config.timeslots[pos].endTime === timeslot.endTime){
           this.config.timeslots.splice(pos,1);
           return true;
@@ -60,14 +80,6 @@ app.service('MampfConfig', function() {
     return false;
   };
 });
-
-/*
-//TODO: separate MampfAPI service necessary?
-app.service('MampfConnection', function(){
-  var url = "http://dennistempi.itm.uni-luebeck.de/mampf/";
-  var api = new MampfAPI(url);
-});
-*/
 
 app.controller('MainController', function($rootScope, $scope, MampfConfig){
 
@@ -88,23 +100,33 @@ app.controller('MainController', function($rootScope, $scope, MampfConfig){
   };
 
   // Mampf Backend Connection
-  $scope.testMampfString = "Press Button!";
   $scope.mampfCon = new MampfAPI(BACKEND_URL);
+  $scope.mampfConfig = MampfConfig;
+
+  // default values for input fields
+  $scope.newInvitee = "Invitee_3";
+  $scope.newTimeslot = {};
+  $scope.newTimeslot.startTime = "2014-05-29T11:22";
+  $scope.newTimeslot.endTime = "2014-05-29T13:22";
   
-  // Fill MampfConfig with demo values (to be deleted)
-  MampfConfig.setIdentity("ich");
-  MampfConfig.addInvitee("invitee1");
-  MampfConfig.addInvitee("invitee2");
-  MampfConfig.setCurrentPos({"longitude" : 9.170299499999999, "latitude" : 48.773556600000006});
-  MampfConfig.addTimeslot({"startTime":"2014-05-28T11:22:03.816+02:00", "endTime":"2014-05-28T13:22:03.817+02:00"});
-  MampfConfig.addTimeslot({"startTime":"2014-05-28T13:22:03.817+02:00", "endTime":"2014-05-28T15:22:03.817+02:00"});
-  console.log(MampfConfig.getConfig());
-  console.log(MampfConfig.config);
+  // Fill MampfConfig with demo values
+  // TODO: delete
+  $scope.mampfConfig.setIdentity("DemoIdentity");
+  $scope.mampfConfig.addInvitee("Invitee_1");
+  $scope.mampfConfig.addInvitee("Invitee_2");
+  $scope.mampfConfig.setCurrentPos({"longitude" : 9.170299499999999, "latitude" : 48.773556600000006});
+  $scope.mampfConfig.addTimeslot({"startTime":"2014-05-28T11:22:03.816+02:00", "endTime":"2014-05-28T13:22:03.817+02:00"});
+  $scope.mampfConfig.addTimeslot({"startTime":"2014-05-28T13:22:03.817+02:00", "endTime":"2014-05-28T15:22:03.817+02:00"});
+
+  console.log($scope.mampfConfig.config);
     
   $scope.testBackendCon = function(){
-    $scope.mampfCon.config = MampfConfig.getConfig();
+    $scope.mampfCon.config = $scope.mampfConfig.config;
     $scope.mampfCon.findMatches();
-    $scope.testMampfString = "Check Console & Network!";
+  };
+
+  $scope.logConfig = function(){
+    console.log($scope.mampfConfig.config);
   };
 
   // Geolocation
@@ -123,6 +145,7 @@ app.controller('MainController', function($rootScope, $scope, MampfConfig){
   $scope.updatePosition = function(position) {
     $scope.currentPosition.latitude = position.coords.latitude;
     $scope.currentPosition.longitude = position.coords.longitude;
+    $scope.mampfConfig.setCurrentPos({"longitude" : position.coords.longitude, "latitude" : position.coords.latitude});
     $scope.$apply();
   };
 
