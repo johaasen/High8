0a6764b84355c1a37348caa6d0b8845e8e216aac
Jan Sosulski <sosulskij@gmail.com>
2014-06-01 13:48:22 +0200

    demoData, stuff

--- js/mampfBackendConnection.js
+++ js/mampfBackendConnection.js
@@ -4,18 +4,21 @@ var MampfAPI = function(apiUrl) {
     this.apiUrl = apiUrl;
     this.config = {};
         
-    this.findMatches = function(async, callback) {
+    this.findMatches = function(callback) {
         var xhr = new XMLHttpRequest();
         
         xhr.onreadystatechange = function() {
             if(xhr.readyState == 4) {
                 if(xhr.status == 200) {
-                    xhr.lastResponse = this.responseText;
+                    console.log(xhr.responseText); //DEBUG
+                    var jsonResponse = JSON.parse(xhr.responseText);
+                    callback(jsonResponse);
                 }
             }
         }
         
         xhr.open("POST",this.apiUrl);
+        xhr.setRequestHeader("Content-type", "application/json");
         xhr.send(JSON.stringify(this.config));
     }
     this.config = {};
@@ -28,48 +31,16 @@ var MampfRequest = function(identity, invitees, currentPosition, timeslots) {
     this.timeslots = timeslots;
 }
 
-/*var JSONmessage = {
-    "identity" : "placeholder",
-    "invitees" : [
-        "placeholder"
-        ],
-    "currentPosition" : {
-        "longitude" : "ph",
-        "latitude" : "ph"
-    },
-    "timeslots" : [
-       {
-          "startTime" : "2014-05-28T11:22:03.816+02:00",
-          "endTime" : "2014-05-28T13:22:03.817+02:00"
-       },
-       {
-          "startTime" : "2014-05-28T13:22:03.817+02:00",
-          "endTime" : "2014-05-28T15:22:03.817+02:00"
-       }
-    ]
-}*/
+function testPrinter(arg) {
+    console.log(arg);
+}
 
-var demoIdentity = "B25BF7426FABCADF01103045FD7707CE";
-var demoInvitees = [
-    "A9B9D2ED66A5DA2AFB3247F6947F5591"
-];
-var demoCurrentPosition = {
-    "longitude" : 9.170299499999999,
-    "latitude" : 48.773556600000006
-};
-var demoTimeSlots = [
-   {
-      "startTime":"2014-05-28T11:22:03.816+02:00",
-      "endTime":"2014-05-28T13:22:03.817+02:00"
-   },
-   {
-      "startTime":"2014-05-28T13:22:03.817+02:00",
-      "endTime":"2014-05-28T15:22:03.817+02:00"
-   }
-];
+var demoConfig1 = {"identity":"B25BF7426FABCADF01103045FD7707CE","invitees":["A9B9D2ED66A5DA2AFB3247F6947F5591"],"currentPosition":{"longitude":9.170299499999999,"latitude":48.773556600000006},"timeslots":[{"startTime":1401621970786,"endTime":1401629170786},{"startTime":1401629170786,"endTime":1401636370786}]};
+
+var demoConfig2 = {"identity":"A9B9D2ED66A5DA2AFB3247F6947F5591","invitees":["B25BF7426FABCADF01103045FD7707CE"],"currentPosition":{"longitude":9.170299499999999,"latitude":48.773556600000006},"timeslots":[{"startTime":1401622489545,"endTime":1401629689545},{"startTime":1401629689545,"endTime":1401636889546}]};
 
 var test = new MampfAPI(BACKEND_URL);
-test.config = new MampfRequest(demoIdentity, demoInvitees, demoCurrentPosition, demoTimeSlots);
+test.config = demoConfig1;
 
 
 
