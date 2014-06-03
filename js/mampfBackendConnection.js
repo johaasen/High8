1b96a5e7b30050f9150bb976aeac7dc8ae9b0679
Jan Sosulski <sosulskij@gmail.com>
2014-06-03 12:34:00 +0200

    removing unnecessary stuff, dedicated debug stuff

--- js/mampfBackendConnection.js
+++ js/mampfBackendConnection.js
@@ -8,11 +8,13 @@ var MampfAPI = function(apiUrl) {
         var xhr = new XMLHttpRequest();
         
         xhr.onreadystatechange = function() {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
+            if(xhr.readyState === 4) {
+                if(xhr.status === 200) {
                     console.log(xhr.responseText); //DEBUG
                     var jsonResponse = JSON.parse(xhr.responseText);
                     callback(jsonResponse);
+                } else {
+                    throw new Error("HTTP STATUS: " + xhr.status);
                 }
             }
         }
@@ -31,9 +33,7 @@ var MampfConfig = function(identity, invitees, currentPosition, timeslots) {
     this.timeslots = timeslots;
 }
 
-function testPrinter(arg) {
-    console.log(arg);
-}
+// DEBUG FROM HERE
 
 var demoConfig1 = {"identity":"B25BF7426FABCADF01103045FD7707CE","invitees":["A9B9D2ED66A5DA2AFB3247F6947F5591"],"currentPosition":{"longitude":9.170299499999999,"latitude":48.773556600000006},"timeslots":[{"startTime":1401621970786,"endTime":1401629170786},{"startTime":1401629170786,"endTime":1401636370786}]};
 
@@ -41,11 +41,3 @@ var demoConfig2 = {"identity":"A9B9D2ED66A5DA2AFB3247F6947F5591","invitees":["B2
 
 var test = new MampfAPI(BACKEND_URL);
 test.config = demoConfig1;
-
-/*
-var apiObject = new MampfAPI(BACKEND_URL);
-apiObject.config = new MampfConfig(identity, invitees, currentPosition, timeslots);
-apiObject.findMatches(callback);
-*/
-
- 
